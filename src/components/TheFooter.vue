a01ae2b7383c14cfc429406ebe842acc8a3bf08a
zhangyong9704 <zhangyong9704@foxmail.com>
2021-01-06 18:31:15 +0800

    初始化环境

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -16,15 +16,6 @@
       <div class="copyright">
         <p class="">
           我们致力于推动网络正版音乐发展，相关版权合作请联系copyrights@kugou.com</p>
-        <p class="text">信息网络传播视听节目许可证 1910564 增值电信业务经营许可证粤B2-20060339 &nbsp;&nbsp;&nbsp;
-          <a >粤ICP备09017694号</a>
-        </p >
-        <p>广播电视节目制作经营许可证（粤）字第01270号&nbsp;&nbsp;&nbsp;&nbsp;营业性演出许可证穗天演440106026&nbsp;&nbsp;&nbsp;
-          <a >营业执照</a>
-        </p>
-        <p>穗公网监备案证第44010650010167&nbsp;&nbsp;&nbsp;&nbsp;互联网药品信息服务资格证编号（粤）-非经营性-2012-0018
-          <a >&nbsp;&nbsp;&nbsp;粤公网安备 44010602000141号</a>
-        </p>
         <p style="-moz-user-select: text;-webkit-user-select: text;-ms-user-select: text; user-select:text">
           <a>互联网不良信息举报中心：www.12377.cn</a>&nbsp;&nbsp;&nbsp;&nbsp;酷狗不良信息举报邮箱：jubao_music@kugou.net&nbsp;&nbsp;&nbsp;&nbsp;客服邮箱：kefu@kugou.net
         </p>
